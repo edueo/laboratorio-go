23d5a91bf75650db4de1ea52ba99b712c132e749
Eduardo Alencar de Oliveira <ealencar10@gmail.com>
2021-07-01 16:13:53 -0300

    feat: hello world
    Primeiro código em go, famoso hello world e script de instalação da ferramenta
    goimports

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # laboratorio-go
+
 Meus expertimentos em golang
+
+## Install `goimports`
+```
+sh install_goimports.sh
+```
---
+++ helloworld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("E tudo começa por um bom e velho hello world!!")
+
+}
